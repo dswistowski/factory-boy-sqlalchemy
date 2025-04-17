4eec6aa973704755808b85dfa0fa041da86d6eda
Damian Świstowski <damian@swistowski.org>
2025-04-17 12:10:52 +0100

    allow SubFactories

--- tests/test_example_orders.py
+++ tests/test_example_orders.py
@@ -5,9 +5,9 @@ from uuid import UUID
 
 import factory as fb
 import pytest
-from sqlalchemy import String
+import sqlalchemy as sa
 from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession, async_sessionmaker
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 from sqlalchemy.types import Text
 
 from fbsa import AsyncFactory, AsyncFactoryMaker
@@ -16,7 +16,7 @@ Str255 = Annotated[str, 255]
 
 
 class Base(DeclarativeBase):
-    type_annotation_map: ClassVar = {Str255: String(255), str: Text()}
+    type_annotation_map: ClassVar = {Str255: sa.String(255), str: Text()}
 
 
 class User(Base):
@@ -28,6 +28,10 @@ class User(Base):
     email: Mapped[Str255]
     address: Mapped[str]
 
+    owned_organizations: Mapped[list["Organization"]] = relationship(
+        back_populates="owner"
+    )
+
 
 class UserFactory(fb.Factory):
     first_name = fb.Faker("first_name")
@@ -37,6 +41,41 @@ class UserFactory(fb.Factory):
 
     class Meta:
         model = User
+
+
+@pytest.fixture
+def async_user_factory(
+    sqlalchemy_async_factory: AsyncFactoryMaker[User],
+) -> AsyncFactory[User]:
+    return sqlalchemy_async_factory(UserFactory)
+
+
+class Organization(Base):
+    __tablename__ = "organization"
+
+    id: Mapped[Annotated[UUID, mapped_column(primary_key=True, default=uuid.uuid4)]]
+    name: Mapped[Str255]
+    owner_id: Mapped[UUID] = mapped_column(
+        sa.ForeignKey("user.id"),
+    )
+    owner: Mapped[User] = relationship(
+        back_populates="owned_organizations", foreign_keys=[owner_id]
+    )
+
+
+class OrganizationFactory(fb.Factory):
+    name = fb.Faker("name")
+    owner = fb.SubFactory(UserFactory)
+
+    class Meta:
+        model = Organization
+
+
+@pytest.fixture
+def async_organization_factory(
+    sqlalchemy_async_factory: AsyncFactoryMaker[Organization],
+) -> AsyncFactory[Organization]:
+    return sqlalchemy_async_factory(OrganizationFactory)
 
 
 @pytest.fixture
@@ -52,13 +91,6 @@ async def async_session(async_db_engine: AsyncEngine) -> AsyncIterable[AsyncSess
             await conn.run_sync(Base.metadata.drop_all)
 
 
-@pytest.fixture
-def async_user_factory(
-    sqlalchemy_async_factory: AsyncFactoryMaker[User],
-) -> AsyncFactory[User]:
-    return sqlalchemy_async_factory(UserFactory)
-
-
 @pytest.mark.asyncio
 async def test_can_create_user(
     async_user_factory: AsyncFactory[User], async_session: AsyncSession
@@ -71,3 +103,17 @@ async def test_can_create_user(
     assert db_user
     assert user.email == db_user.email
     assert db_user.first_name == "Joe"
+
+
+@pytest.mark.asyncio
+async def test_can_create_organization(
+    async_organization_factory: AsyncFactory[Organization],
+    async_session: AsyncSession,
+) -> None:
+    organization = await async_organization_factory(owner__first_name="Joe")
+
+    assert organization.id
+
+    db_organization = await async_session.get(Organization, organization.id)
+    assert db_organization
+    assert db_organization.owner.first_name == "Joe"
